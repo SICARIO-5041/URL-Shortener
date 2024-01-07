61310cd8cad31cdb383cb7db482970286c7e0e4f
Eduard Kalaichev <155888637+SICARIO-5041@users.noreply.github.com>
2024-01-07 19:58:12 +0300

    Add files via upload

---
+++ url-shortener/cmd/url-shortener/config/internal/config/config.go
@@ -0,0 +1,17 @@
+package config 
+
+type Config struct {
+	Env string `yaml:"env" env:"ENV" env-deafault:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Adress string `yaml:"adress" env-default:"localhost:8080"`
+	Timeout time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default: "60s"`
+}
+
+func MustLoad() {
+	configPath := os.Getenv( key: "CONFIG_PATH")
+}
